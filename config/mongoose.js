d55f81bec593ae881f5db43481367d9650f2c6d2
RohanVashisht003 <sharmarohan544@gmail.com>
2022-05-27 23:42:58 +0530

    mongoose middleware setup

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// connect to db
+mongoose.connect('mongodb://localhost/hospital_db');
+
+// acquire the connection to check if it is successful or not
+const db = mongoose.connection;
+
+// if error
+db.on('error',console.error.bind(console, "Error in connecting to db"));
+
+// if successfully working
+db.once('open', ()=>{
+    console.log("Successfully connected to db");
+})
+
+
+module.exports = db;
